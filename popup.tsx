4e3f8dbe5c6274677d8f9670848ce1c96017b5d6
NubPlayz <182065584+NubPlayz@users.noreply.github.com>
2026-03-18 18:40:43 +0530

    added popup page

--- popup.tsx
+++ popup.tsx
@@ -1,24 +1,23 @@
-import { useState } from "react"
+import "./popup.css"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
-
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="popup">
+      <h2 className="popup-title">Goodlib</h2>
+      <div className="popup-card">
+        <div className="popup-row">
+          <div className="popup-avatar">
+            Z
+          </div>
+          <div className="popup-copy">
+            <div className="popup-name">Z-Lib</div>
+            <div className="popup-subtitle">z-lib.gl</div>
+          </div>
+          <label className="popup-toggle">
+            <input type="checkbox" defaultChecked  />
+          </label>
+        </div>
+      </div>
     </div>
   )
 }
